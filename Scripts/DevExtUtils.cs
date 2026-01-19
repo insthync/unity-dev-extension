c17d905ef1bd67adfd29fc0833afd77025f2db90
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2026-01-20 06:08:54 +0700

    feat: try to reduce GC.Alloc()

--- Scripts/DevExtUtils.cs
+++ Scripts/DevExtUtils.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Reflection;
 #if !NET && !NETCOREAPP
 using UnityEngine;
+using UnityEngine.Profiling;
 #endif
 
 namespace Insthync.DevExtension
@@ -48,23 +49,47 @@ namespace Insthync.DevExtension
         private static bool TryGetDevExtMethods(Type type, string baseMethodName, BindingFlags bindingFlags, out MethodInfo[] methods)
         {
             methods = null;
-            DevExtMethodsAttribute tempAttribute;
             string typeName = type.FullName;
-            if (!s_cacheDevExtMethods.ContainsKey(typeName) || !s_cacheDevExtMethods[typeName].ContainsKey(baseMethodName))
+
+            if (!s_cacheDevExtMethods.TryGetValue(typeName, out var methodDict))
             {
-                if (!s_cacheDevExtMethods.ContainsKey(typeName))
-                    s_cacheDevExtMethods.Add(typeName, new Dictionary<string, MethodInfo[]>());
-                s_cacheDevExtMethods[typeName].Add(baseMethodName, null);
-                methods = type.GetMethods(bindingFlags).Where(a =>
-                {
-                    tempAttribute = (DevExtMethodsAttribute)a.GetCustomAttribute(typeof(DevExtMethodsAttribute), true);
-                    return tempAttribute != null && tempAttribute.BaseMethodName.Equals(baseMethodName);
-                }).ToArray();
-                s_cacheDevExtMethods[typeName][baseMethodName] = methods;
+                methodDict = new Dictionary<string, MethodInfo[]>();
+                s_cacheDevExtMethods[typeName] = methodDict;
             }
-            methods = s_cacheDevExtMethods[typeName][baseMethodName];
-            return methods != null && methods.Length > 0;
+
+            if (!methodDict.TryGetValue(baseMethodName, out methods))
+            {
+                var allMethods = type.GetMethods(bindingFlags); // allocates once
+                int count = 0;
+
+                // Use a preallocated temp array (worst-case: all methods match)
+                MethodInfo[] tempMethods = new MethodInfo[allMethods.Length];
+
+                for (int i = 0; i < allMethods.Length; i++)
+                {
+                    var attr = (DevExtMethodsAttribute)allMethods[i].GetCustomAttribute(typeof(DevExtMethodsAttribute), true);
+                    if (attr != null && string.Equals(attr.BaseMethodName, baseMethodName))
+                    {
+                        tempMethods[count++] = allMethods[i];
+                    }
+                }
+
+                if (count == 0)
+                    methods = Array.Empty<MethodInfo>();
+                else if (count == allMethods.Length)
+                    methods = tempMethods; // all methods matched
+                else
+                {
+                    methods = new MethodInfo[count];
+                    Array.Copy(tempMethods, 0, methods, 0, count);
+                }
+
+                methodDict[baseMethodName] = methods;
+            }
+
+            return methods.Length > 0;
         }
+
 
         private static void InvokeDevExtMethods(Type type, object obj, string baseMethodName, BindingFlags bindingFlags, params object[] args)
         {
